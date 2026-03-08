a2f938bef464db53ce2be202f77f4c5865d80d73
Rithvik-Chikatla <98206714+Rithvik-Chikatla@users.noreply.github.com>
2026-03-09 00:19:52 +0530

    Second commit: React Quiz

---
+++ src/components/App.js
@@ -0,0 +1,102 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        // we updated 2 states in one go using reducer, which is not possible with useState
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { questions, status, index, answer } = state;
+
+  const totalQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen totalQuestions={totalQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              totalQuestions={totalQuestions}
+              index={index}
+            />
+          </>
+        )}
+      </Main>
+    </div>
+  );
+}
---
+++ src/components/Options.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${answer === index ? "answer" : ""} ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
